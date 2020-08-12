bf3134a8ee5a700e8aa3cc396b87b9b6ebf490c3
MutiaNandhika <mutianandhika@gmail.com>
2020-08-12 12:50:25 +0700

    new boolean

--- Kotlin Fundamental/src/main/kotlin/Boolean.kt
+++ Kotlin Fundamental/src/main/kotlin/Boolean.kt
@@ -1,47 +1,17 @@
-fun main() {
+fun main(){
+    val nilai = 65
+    val score : Char
 
-    var nilai : Int = 90
-    val tuntas = nilai >= 75
-    if (!tuntas)
-    {
-        println("Nilai kamu $nilai, Silahkan ikut remidial.")
+    score = if (nilai > 90){
+        'A'
+    }else if (nilai <=90 && nilai >80){
+        'B'
+    }else if (nilai <= 80 && nilai > 70){
+        'C'
+    }else if (nilai <= 70 && nilai >60){
+        'D'
+    }else{
+        'E'
     }
-    else
-    {
-        println("Nilai kamu $nilai, Selamat ya!")
-    }
-
-    val a = 90
-    val b = 80
-    val c = 70
-    val d = 60
-    val e = 50
-
-    if (nilai > a)
-    {
-        println("A" )
-
-    }
-    else if (nilai > b) {
-
-        println("Nilai kamu B")
-    }
-    else if (nilai > c){
-
-        println("Nilai kamu C")
-    }
-    else if (nilai > d){
-
-        println("Nilai kamu D")
-    }
-    else if (nilai <= e){
-
-        println("Nilai kamu E")
-
-    }
-    else {
-        println("Tidak tersedia")
-    }
+    println("Nilaimu $score")
 }
-
-
